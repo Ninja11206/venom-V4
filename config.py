d14a2e5460b6fb94c688e1a2cc70f3254d7aba6a
Ninja11206 <rajdabi837@gmail.com>
2024-08-06 11:09:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7411721555:AAFhsckG1U6_nzm9JjMuD4iYSMvO7HpgG1M")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Supernova1212")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1426717719")
 
 
 
